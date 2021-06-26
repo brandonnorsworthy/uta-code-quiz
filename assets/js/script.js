2f1151ce62e94aa03d465b44fcc668391d217f70
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-06-26 17:01:17 -0500

    update readme and build out order of operations for js

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+
+function init() {
+
+}
+
+init();
+
+/*
+1. list of questions with answers
+2. test input against answer
+3a.show new question with its own answers
+3. add timer
+4. if out of questions game over
+5. if timer 0 game over
+5. if correct do nothing if incorrect subtract time
+*/
